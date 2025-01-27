486beb54018e526d05d97347b9d1707ea5cee02b
Piotr Krysik <ptrkrysik@gmail.com>
2025-01-27 19:55:47 +0100

    Add workaround for fans on GXxHRXx laptop

--- src/tuxedo_io/tuxedo_io.c
+++ src/tuxedo_io/tuxedo_io.c
@@ -151,15 +151,6 @@ static int tdp_max_x6fr5xx[] = { 0xa2, 0xa2, 0xc3 };
 static int tdp_min_xxxx45xs[] = { 0x0a, 0x0a, 0x0a };
 static int tdp_max_xxxx45xs[] = { 0x64, 0x64, 0x69 };
 
-static int tdp_min_x6ar55xu[] = { 0x0a, 0x0a, 0x0a };
-static int tdp_max_x6ar55xu[] = { 0x91, 0x9b, 0x122 };
-
-static int tdp_min_x5ar45xs[] = { 0x0a, 0x0a, 0x0a };
-static int tdp_max_x5ar45xs[] = { 0x5a, 0x5a, 0xe6 };
-
-static int tdp_min_x6xx45xu[] = { 0x0a, 0x0a, 0x0a };
-static int tdp_max_x6xx45xu[] = { 0x64, 0x64, 0x69 };
-
 static int *tdp_min_defs = NULL;
 static int *tdp_max_defs = NULL;
 
@@ -246,15 +237,6 @@ static void uw_id_tdp(void)
 	} else if (dmi_match(DMI_BOARD_NAME, "X5KK45xS_X5SP45xS")) {
 		tdp_min_defs = tdp_min_xxxx45xs;
 		tdp_max_defs = tdp_max_xxxx45xs;
-	} else if (dmi_match(DMI_BOARD_NAME, "X6AR55xU")) {
-		tdp_min_defs = tdp_min_x6ar55xu;
-		tdp_max_defs = tdp_max_x6ar55xu;
-	} else if (dmi_match(DMI_BOARD_NAME, "X5AR45xS")) {
-		tdp_min_defs = tdp_min_x5ar45xs;
-		tdp_max_defs = tdp_max_x5ar45xs;
-	} else if (dmi_match(DMI_BOARD_NAME, "X6KK45xU_X6SP45xU")) {
-		tdp_min_defs = tdp_min_x6xx45xu;
-		tdp_max_defs = tdp_max_x6xx45xu;
 #endif
 	} else {
 		tdp_min_defs = NULL;
